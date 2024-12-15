ae2e8eaaeca75fdc0f9f3324a94c12ef3e06d51a
Kavya Singh <kavya_singh@kavya.singh>
2024-12-16 00:27:41 +0530

    basics scripts

---
+++ first-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir kavya
+cd kavya
+touch hello.txt
+echo "some data for the file" >> hello.txt
+cat hello.txt
---
+++ second-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ./kavya/
+rm hello.txt
+cd ..
+rmdir kavya
+
